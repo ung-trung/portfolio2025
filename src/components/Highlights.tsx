d7826e868eda224d0f51761badbef86957a3d6a0
Trung Ung <ung.trung@nordcloud.com>
2025-04-03 17:43:42 +0300

    update highlight sections

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -4,34 +4,24 @@ export const Highlights = () => {
   return (
     <section id="highlights" className="mb-16">
       <div className="text-primary-foreground/70 flex flex-col justify-between gap-4 sm:flex-row">
-        <div className="text-xs font-bold">
+        <div className="hover:text-primary-foreground text-xs font-bold">
           <Briefcase className="inline-block" /> 5+ years experience
         </div>
-        <div className="text-xs font-bold">
-          <ShieldCheck className="inline-block" />{" "}
-          <a
-            className="underline"
-            href="https://learn.microsoft.com/api/credentials/share/en-us/ungkientrung/80B2DBEFDEA4B25?sharingId=B3FE2FE1B9855E8"
-            target="_blank"
-          >
-            Azure
-          </a>{" "}
-          &{" "}
-          <a
-            className="underline"
-            href="https://www.credly.com/badges/de2977b7-a2c3-4b80-9c53-d9b1279b9689/public_url"
-            target="_blank"
-          >
-            AWS
-          </a>{" "}
-          certified
-        </div>
-        <div className="text-xs font-bold">
+        <a
+          href="#certificates"
+          className="hover:text-primary-foreground text-xs font-bold"
+        >
+          <ShieldCheck className="inline-block" /> Azure & AWS certified
+        </a>
+        <div className="hover:text-primary-foreground text-xs font-bold">
           <Award className="inline-block" /> 2× award winner
         </div>
-        <div className="text-xs font-bold">
+        <a
+          href="#projects"
+          className="hover:text-primary-foreground text-xs font-bold"
+        >
           <PackageOpen className="inline-block" /> 6+ products delivered
-        </div>
+        </a>
       </div>
     </section>
   );
