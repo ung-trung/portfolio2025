5287ada4e258874d8a53c5c98c90507a6a94faad
Trung Ung <ung.trung@nordcloud.com>
2025-04-04 14:36:04 +0300

    remove text animation on certificates

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -34,7 +34,7 @@ export const Certificates = () => {
     },
   ];
   return (
-    <section id="certificates" className="relative mt-20 mb-16 scroll-mt-20">
+    <section id="certificates" className="mt-20 mb-16 scroll-mt-20">
       <h2 className="mb-2 box-decoration-clone bg-clip-text text-[1.7rem] font-bold">
         Certificates
         <span className="bg-gradient-to-r from-yellow-300 to-yellow-600 box-decoration-clone bg-clip-text text-transparent">
@@ -56,7 +56,6 @@ export const Certificates = () => {
           </div>
         ))}
       </div>
-      <div aria-hidden className="color-rays h-full w-full mix-blend-screen" />
     </section>
   );
 };
